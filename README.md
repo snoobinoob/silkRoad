ac526b29619ee0ddeeb36d8bff9042edb54afbd9
Snoobinoob <cheeseycheese99@gmail.com>
2023-02-19 17:29:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+An inter-settlement trading mod for Necesse.
+
+Adds a new `Port` object to the game.
+
+Check out the [modding wiki page](https://necessewiki.com/Modding) for more.
---
+++ src/main/java/silkRoad/SilkRoad.java
@@ -0,0 +1,11 @@
+package silkRoad;
+
+import necesse.engine.modLoader.annotations.ModEntry;
+
+@ModEntry
+public class SilkRoad {
+
+    public void init() {
+        System.out.println("Hello world from Silk Road!");
+    }
+}
